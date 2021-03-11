1ae5cdd3f70b0bfb9705bf17ff2723371c5c6fba
Raffaele-ProgramSite <62758048+Raffaele-ProgramSite@users.noreply.github.com>
2021-03-11 09:15:25 +0100

    Aggiunto Sito di Minecraft Italia ai Vote

--- src/scrumblernetwork/voteCommand.java
+++ src/scrumblernetwork/voteCommand.java
@@ -17,15 +17,17 @@ public class voteCommand extends Command {
                 ProxiedPlayer p = (ProxiedPlayer) cs;
                 p.sendMessage("§7§l§m--------------------------------------------");
                 p.sendMessage("§8§l- §7Siti per Votare il Server:");
-                p.sendMessage("§8§l- §71) §fmcserveritalia.it");
-                p.sendMessage("§8§l- §72) §fhttps://minecraft-mp.com/server-s277712");
+                p.sendMessage("§8§l- §71) §fhttps://minecraft-italia.it/server/scrumblernetwork");
+                p.sendMessage("§8§l- §72) §fmcserveritalia.it");
+                p.sendMessage("§8§l- §73) §fhttps://minecraft-mp.com/server-s277712");
                 p.sendMessage("§7§l§m--------------------------------------------");
             }
         }else{
             System.out.print("--------------------------------------------");
             System.out.print("- Siti per Votare il Server:");
-            System.out.print("- 1) mcserveritalia.it");
-            System.out.print("- 2) https://minecraft-mp.com/server-s277712");
+            System.out.print("- 1) https://minecraft-italia.it/server/scrumblernetwork");
+            System.out.print("- 2) mcserveritalia.it");
+            System.out.print("- 3) https://minecraft-mp.com/server-s277712");
             System.out.print("--------------------------------------------");
         }
     }
